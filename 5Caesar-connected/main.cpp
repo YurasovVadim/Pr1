5f36fb9059cedb0488a8fd4890b8b5f978ec0f25
VadYurasov <Yurasov.02@mail.ru>
2021-04-09 11:39:44 +0300

    Доработка

--- 5Caesar-connected/main.cpp
+++ 5Caesar-connected/main.cpp
@@ -7,13 +7,14 @@
 #include <regex>
 using namespace std;
 
-string coder(string s, int key, int i)
+string decoder(string s, int key)
 {
+    int i=0;
 s = regex_replace(s, regex("№"), " ");
     for (int i = 0; s[i] != '\0'; i++) {
         if (isalnum(s[i]) || ispunct (s[i]) || isblank (s[i])) {
 cout << "Ошибка"<<endl;
-return 0;
+return s;
         }
     }
 // Возведение к верхнему регистру
@@ -37,38 +38,37 @@ return 0;
  return s;
 }
 
-string decoder(string s, int key, int i)
+string coder(string s, int key)
 {
-s = regex_replace(s, regex("№"), " ");
-    for (int i = 0; s[i] != '\0'; i++) { 
-        if (isalnum(s[i]) || ispunct (s[i]) || isblank (s[i])) {
-cout << "Ошибка"<<endl;
-return 0;
+ int i=0;
+ s = regex_replace(s, regex("№"), " ");
+    for (int i = 0; s[i] != '\0'; i++) { //Пробегаем по строке циклом (слева направо)
+        if (isalnum(s[i]) || ispunct (s[i]) || isblank (s[i])) { //Условие, что символ не является буквой или цифрой
+            s.erase(i, 1); //Удаляем найденный символ
+            i--; //Сдвигаем индекс после удаления
         }
     }
- locale loc("ru_RU.UTF-8");
- wstring_convert<codecvt_utf8<wchar_t>, wchar_t> codec;
- wstring ws = codec.from_bytes(s);
+ // Возведение к верхнему регистру
+ locale loc("ru_RU.UTF-8"); // русская локаль для корректной смены регистра
+ wstring_convert<codecvt_utf8<wchar_t>, wchar_t> codec; //кодек UTF-8
+ wstring ws = codec.from_bytes(s); // перекодируем из UTF-8 in UTF-32
  for (wchar_t & wc: ws) {
  wc = toupper(wc,loc);
- }
+  }
  int size;
  size=ws.size();
  for (i=0; i<size; i++) {
- ws[i]=ws[i] - key%32
- ;
- if (ws[i]>=1072) ws[i]=ws[i]-32;
+            ws[i]=ws[i] + key%32;
+ if (ws[i]>=1072) ws[i]=ws[i]-32; 
 
  }
- s = codec.to_bytes(ws);
- cout<< "Расшифрованная строка :"<< endl;
+ s = codec.to_bytes(ws); // перекодируем из UTF-32 in UTF-8
  cout << s << "\n";
 
  return s;
 }
 int main()
 {
- int i=0;
  string s;
  cout <<"Введите строку: ";
  getline(cin, s);
@@ -80,11 +80,11 @@ int main()
  cin>>dei;
  string new_s;
  if (dei==0) {
- coder(s,key, i);
+ coder(s,key);
  } else if (dei == 1) {
- decoder(s,key, i);
+ decoder(s,key);
  } else {
  cout<<"Данного действия не существует"<<endl;
- return 0;
  }
+  return 0;
 }
